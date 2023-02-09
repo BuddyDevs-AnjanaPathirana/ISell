935079a2e2dba56bdcc3393f0fe33a1db0bec258
Änjana Pathirana <anjanamadhushan44@gmail.com>
2023-02-09 17:04:34 +0530

    Day end commit

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
+import { View } from "react-native";
+import AppButton from "./app/components/AppButton";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <WelcomeScreen />;
 }
