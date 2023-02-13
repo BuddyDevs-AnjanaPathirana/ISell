5f8c4f384b12fa3d9f3ae665e949ab5552d7ae57
Änjana Pathirana <anjanamadhushan44@gmail.com>
2023-02-13 16:53:30 +0530

    Day end commit

--- App.js
+++ App.js
@@ -1,18 +1,25 @@
 import { Image, SafeAreaView, StyleSheet, View } from "react-native";
 import AppButton from "./app/components/AppButton";
 import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.cardPage}>
-      <Card
-        title="Red jacket for sale!"
-        subTitle="$100"
-        image={require("./app/assets/jacket.jpg")}
-      />
-    </SafeAreaView>
+    // <SafeAreaView style={styles.cardPage}>
+    //   <Card
+    //     title="Red jacket for sale!"
+    //     subTitle="$100"
+    //     image={require("./app/assets/jacket.jpg")}
+    //   />
+    // </SafeAreaView>
+    // <ListingDetailsScreen
+    //   title="Red jacket for sale!"
+    //   subTitle="$100"
+    //   image={require("./app/assets/jacket.jpg")}
+    // />
+    <ViewImageScreen></ViewImageScreen>
   );
 }
 
